f5b3546cb74b4325a25fd4adce213c621b47b84a
piotrmachut <piotr.machut@gmail.com>
2021-04-10 16:33:02 +0200

    Add player's character.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+# Initialize game window, set window title and icon:
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
@@ -7,6 +8,17 @@ pygame.display.set_caption("Turtle Warrior")
 
 icon = pygame.image.load("assets/graphics/icon.png")
 pygame.display.set_icon(icon)
+
+# Load player's character and set its' basic position:
+player_img = pygame.image.load("assets/graphics/player_donatello.png")
+player_x = 368
+player_y = 480
+player_speed = 0
+
+
+def player(x, y):
+    screen.blit(player_img, (x, y))
+
 
 running = True
 
@@ -16,5 +28,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_speed = -0.1
+            if event.key == pygame.K_RIGHT:
+                player_speed = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_speed = 0
+
+    player_x += player_speed
+
+    # Set game area for player:
+    if player_x <= 0:
+        player_x = 0
+    elif player_x >= 736:
+        player_x = 736
+
+    player(player_x, player_y)
 
     pygame.display.update()
