3de6da15202afbf6b90717132898482e56e764a8
piotrmachut <piotr.machut@gmail.com>
2021-04-10 15:51:12 +0200

    Set game caption, game icon and window background color.

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Turtle Warrior")
+
+icon = pygame.image.load("assets/graphics/icon.png")
+pygame.display.set_icon(icon)
 
 running = True
 
 while running:
+    screen.fill((71, 57, 39))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    pygame.display.update()
