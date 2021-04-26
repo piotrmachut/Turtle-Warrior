d61d749c31f4a2ee25e1517595129ed06c1a9a12
piotrmachut <piotr.machut@gmail.com>
2021-04-26 22:22:08 +0200

    Update character's vertical and horizontal movements.

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ pygame.display.set_caption("Turtle Warrior")
 icon = pygame.image.load("assets/graphics/icon.png")
 pygame.display.set_icon(icon)
 
-# Load player's character and set its' basic position:
+# Load player's character image and set its' basic position:
 player_img = pygame.image.load("assets/graphics/player_donatello.png")
 player_x = 368
 player_y = 480
-player_speed = 0
+player_speed_x = 0
+player_speed_y = 0
 
 
 def player(x, y):
@@ -23,21 +24,30 @@ def player(x, y):
 running = True
 
 while running:
+    # Game screen background color
     screen.fill((71, 57, 39))
 
+    # Player movements
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_speed = -0.1
+                player_speed_x = -0.1
             if event.key == pygame.K_RIGHT:
-                player_speed = 0.1
+                player_speed_x = 0.1
+            if event.key == pygame.K_UP:
+                player_speed_y = -0.1
+            if event.key == pygame.K_DOWN:
+                player_speed_y = 0.1
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                player_speed = 0
+                player_speed_x = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                player_speed_y = 0
 
-    player_x += player_speed
+    player_x += player_speed_x
+    player_y += player_speed_y
 
     # Set game area for player:
     if player_x <= 0:
@@ -45,6 +55,12 @@ while running:
     elif player_x >= 736:
         player_x = 736
 
+    if player_y <= 0:
+        player_y = 0
+    elif player_y >= 536:
+        player_y = 536
+
+    # make magic come true: run player character function
     player(player_x, player_y)
 
     pygame.display.update()
